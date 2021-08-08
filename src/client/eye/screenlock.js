d78fb546bce213592d9ddf33da225b8731309ac3
Mike Patterson <michaelandrewpatterson@gmail.com>
2021-08-07 22:52:13 -0500

    Add error handling around screen locking/unlocking

--- src/client/eye/screenlock.js
+++ src/client/eye/screenlock.js
@@ -2,24 +2,32 @@
 	let wakeLock;
 
 	async function lock() {
-		if (wakeLock) {
-			return;
+		try {
+			if (wakeLock) {
+				return;
+			}
+	
+			wakeLock = await navigator.wakeLock.request("screen");
+			wakeLock.addEventListener("release", () => {
+				console.log("Screen unlocked.");
+			});
+			console.log("Screen locked.");
+		} catch (e) {
+			console.warn(`Error occurred attempting to lock screen. ${e.message}`);
 		}
-
-		wakeLock = await navigator.wakeLock.request("screen");
-		wakeLock.addEventListener("release", () => {
-			console.log("Screen unlocked.");
-		});
-		console.log("Screen locked.");
 	}
 
 	async function unlock() {
-		if (!wakeLock) {
-			return;
+		try {
+			if (!wakeLock) {
+				return;
+			}
+	
+			await wakeLock.release();
+			wakeLock = undefined;
+		} catch (e) {
+			console.warn(`Error occurred attempting to unlock screen. ${e.message}`);
 		}
-
-		await wakeLock.release();
-		wakeLock = undefined;
 	}
 
 	const api = {
